0245161793bc8ef4ecea33a4b5af57d58e1aefe1
Amit Chavda <amit.c@simformsolutions.com>
2022-05-03 16:56:10 +0530

    main class name changed

--- src/main/java/com/proxibid/Proxibid.java
+++ src/main/java/com/proxibid/Proxibid.java
@@ -4,23 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 import com.proxibid.controller.WelcomeController;
-
 import java.io.File;
 
+@SpringBootConfiguration
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
-@SpringBootConfiguration
-public class Proffer {
+public class Proxibid {
 
 	public static void main(String[] args) {
 		new File(WelcomeController.uploadDirectory).mkdir();
 		new File(WelcomeController.uploadDirectoryForCatalog).mkdir();
-		SpringApplication.run(Proffer.class, args);
+		SpringApplication.run(Proxibid.class, args);
 	}
 
 }
